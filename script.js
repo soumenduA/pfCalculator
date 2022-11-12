94e77dfa289094a46bca829f6eac90a0fb3a9179
SoumenduA <soumendu.adhikary@gmail.com>
2022-11-12 20:58:09 +0530

    Month field uneditable, new before/after fields

--- script.js
+++ script.js
@@ -24,9 +24,14 @@ $(document).ready(() => {
     }
     return null;
   };
+  
+  const getCurrentMonthIndex = () => {
+    // TODO
+  }
 
   $("#btn-export").click(function () {
     console.log("export clicked");
+    $(".header-line-breaker").remove();
     $("#PF_Table").table2excel({
       // exclude CSS class
       exclude: ".noExl",
@@ -37,24 +42,21 @@ $(document).ready(() => {
       exclude_links: true,
       exclude_inputs: true,
     });
-  });
-
-  $(".input-field").change(function () {
-    let value1 = $(this).val();
-    $(this).val(value1);
-    console.log(value1);
+    location.reload();
   });
 
   $("#next_btn").click(function () {
-    const month = $("#month").val();
-    const index = months.findIndex((ele) => ele.month === month);
+    // const month = $("#month").val();
+    const month = $("#month").html(); //change for making month uneditable
+    const index = months.findIndex((ele) => ele.month.toLowerCase() === month.toLowerCase()); //change for making month uneditable
     if (months[index].isAddedToTable) {
       const nextMonth = getNextMonth(month.toLowerCase());
       console.log({ clBal, opBal, nextMonth });
       opBal = clBal;
       $("#opening-bal").val(opBal);
       nextMonth
-        ? $("#month").val(nextMonth)
+        // ? $("#month").val(nextMonth)
+        ? $("#month").html(nextMonth.toUpperCase())
         : window.alert(
             "Done for the FY! Please reload the page to enter new data."
           );
@@ -62,40 +64,47 @@ $(document).ready(() => {
   });
 
   $("#addToTable_btn").click(function () {
-    const month = $("#month").val();
-    console.log("adding row to table", month);
-    const index = months.findIndex((ele) => ele.month === month);
-    if (!months[index].isAddedToTable) {
-      try {
-        opBal = parseFloat($("#opening-bal").val(), 2);
-        const bfr_15 = parseFloat($("#before-15").val(), 2);
-        const aft_15 = parseFloat($("#after-15").val(), 2);
-        const intPA = parseFloat($("#int-pa").val(), 2);
-        const pfReco = parseFloat($("#pf-reco").val(), 2);
-        const pfAdv = parseFloat($("#pf-adv").val(), 2);
-        const lwstPayment = opBal + bfr_15;
-        const intr = (lwstPayment * intPA) / 1200;
-        clBal = opBal + bfr_15 + aft_15 + pfReco - pfAdv;
-        console.log({ clBal, opBal, intr });
-        $("#PF_Table tr:last").after(
-          `<tr id=${month.toLowerCase()}>
-          <td>${month.toUpperCase()}</td>
-          <td>${opBal}</td>
-          <td>${bfr_15}</td>
-          <td>${aft_15}</td>
-          <td>${pfAdv}</td>
-          <td>${pfReco}</td>
-          <td>${lwstPayment}</td>
-          <td>${intPA}</td>
-          <td>${intr.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]}</td>
-          <td>${clBal.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]}</td>
-          </tr>`
-        ); // two places after decimal truncation is handled using string regex matcher
-        months[index].isAddedToTable = true;
-      } catch (err) {
-        window.alert("Something went WRONG! Please check if all fields are filled.");
-        // console.error(err);
-      }
-    } else window.alert(`${month.toUpperCase()} data already added to table, please go to next month.`);
+    // const month = $("#month").val();
+      const month = $("#month").html(); //change for making month uneditable
+      console.log("adding row to table", month);
+      const index = months.findIndex((ele) => ele.month.toLowerCase() === month.toLowerCase()); //change for making month uneditable
+      if (!months[index].isAddedToTable) {
+        if(window.confirm("Are you sure you wnat to add this row to table?")){
+          try {
+            opBal = parseFloat($("#opening-bal").val(), 2);
+            const bfr_15 = parseFloat($("#before-15").val() || 0, 2);
+            const aft_15 = parseFloat($("#after-15").val() || 0, 2);
+            const intPA = parseFloat($("#int-pa").val(), 2);
+            const pfRecoBfr = parseFloat($("#pf-reco-bfr").val() || 0, 2);
+            const pfAdvBfr = parseFloat($("#pf-adv-bfr").val() || 0, 2);
+            const pfRecoAft = parseFloat($("#pf-reco-aft").val() || 0, 2);
+            const pfAdvAft = parseFloat($("#pf-adv-aft").val() || 0, 2);
+            const lwstPayment = opBal + bfr_15 + pfRecoBfr - pfAdvBfr;
+            const intr = (lwstPayment * intPA) / 1200;
+            clBal = opBal + bfr_15 + aft_15 + pfRecoBfr + pfRecoAft - pfAdvBfr - pfAdvAft;
+            console.log({ clBal, opBal, intr, bfr_15, aft_15 });
+            $("#PF_Table tr:last").after(
+              `<tr id=${month.toLowerCase()}>
+              <td>${month.toUpperCase()}</td>
+              <td>${opBal}</td>
+              <td>${bfr_15}</td>
+              <td>${aft_15}</td>
+              <td>${pfAdvBfr}</td>
+              <td>${pfAdvAft}</td>
+              <td>${pfRecoBfr}</td>
+              <td>${pfRecoAft}</td>
+              <td>${lwstPayment}</td>
+              <td>${intPA}</td>
+              <td>${intr.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]}</td>
+              <td>${clBal.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]}</td>
+              </tr>`
+            ); // two places after decimal truncation is handled using string regex matcher
+            months[index].isAddedToTable = true;
+          } catch (err) {
+            window.alert("Something went WRONG! Please check if all fields are filled.");
+            // console.error(err);
+          }
+        }
+      } else window.alert(`${month.toUpperCase()} data already added to table, please go to next month.`);
   });
 });
