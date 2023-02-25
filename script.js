447f4c2a3c1bbfba131882aed595ef2efa51a3c7
soumenduA <45918869+soumenduA@users.noreply.github.com>
2023-02-25 23:05:51 +0530

    Develop (#4)
    * Total row added
    
    * minor fix
    
    * WIP - employee related fields
    
    * MVP 0.1 with all basic features

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(() => {
   let clBal = 0;
   let intrSum = 0;
   let totalSum = 0;
+  var fileName = "";
   const months = [
     { month: "apr", isAddedToTable: false },
     { month: "may", isAddedToTable: false },
@@ -20,7 +21,6 @@ $(document).ready(() => {
 
   const getNextMonth = (currMonth) => {
     const index = months.findIndex((ele) => ele.month === currMonth);
-    console.log({ index });
     if (index >= 0 && index < months.length - 1) {
       return months[index + 1].month;
     }
@@ -29,17 +29,59 @@ $(document).ready(() => {
 
   const getCurrentMonthIndex = () => {
     // TODO
-  }
+  };
+  const schools = [
+    { name: "None", value: "None", addr: "none" },
+    {
+      name: "SANTOSHPUR GOVT. COLONY NETAJI SUBHAS VIDYALAYA(H.S)",
+      id: "SANTOSHPUR_GOVT",
+      value: "SANTOSHPUR GOVT. COLONY NETAJI SUBHAS VIDYALAYA(H.S)",
+      addr: "P.O.-SANTOSHPUR(M), P.S.-MAHASHTALA, DIST.-24PGS(SOUTH), PIN - 700044",
+    },
+  ];
+
+  schools.forEach((ele) => {
+    $("#school-name-inpt").append(`<option value="${ele.value}">
+      ${ele.name}
+    </option>`);
+  });
+
+  $("#school-address-inpt").append(`<option value="None">
+    ${"None"}
+  </option>`);
+
+  $("#school-name-inpt").change(function (event) {
+    schools.forEach((ele) => {
+      if (ele.value === event.target.value) {
+        $("#school-address-inpt").append(`<option value="${ele.addr}">
+          ${ele.addr}
+        </option>`);
+      }
+    });
+    $("#school-name").html(event.target.value);
+  });
+
+  $("#school-address-inpt").change(function (event) {
+    $("#school-address").html(event.target.value);
+  });
+
+  $("#emp-name-inpt").change(function (event) {
+    $("#emp-name").html(event.target.value.toUpperCase());
+    fileName = event.target.value.toUpperCase();
+  });
+
+  $("#emp-designation-inpt").change(function (event) {
+    $("#emp-designation").html(event.target.value.toUpperCase());
+  });
 
   $("#btn-export").click(function () {
-    console.log("export clicked");
     $(".header-line-breaker").remove();
     $("#PF_Table").table2excel({
       // exclude CSS class
       exclude: ".noExl",
       name: "Worksheet Name",
-      filename: "Employee", //do not include extension
-      fileext: ".xlsx", // file extension
+      filename: `${fileName}`, //do not include extension
+      fileext: ".xls", // file extension
       exclude_img: true,
       exclude_links: true,
       exclude_inputs: true,
@@ -50,15 +92,16 @@ $(document).ready(() => {
   $("#next_btn").click(function () {
     // const month = $("#month").val();
     const month = $("#month").html(); //change for making month uneditable
-    const index = months.findIndex((ele) => ele.month.toLowerCase() === month.toLowerCase()); //change for making month uneditable
+    const index = months.findIndex(
+      (ele) => ele.month.toLowerCase() === month.toLowerCase()
+    ); //change for making month uneditable
     if (months[index].isAddedToTable) {
       const nextMonth = getNextMonth(month.toLowerCase());
-      console.log({ clBal, opBal, nextMonth });
       opBal = clBal;
       $("#opening-bal").val(opBal);
       nextMonth
-        // ? $("#month").val(nextMonth)
-        ? $("#month").html(nextMonth.toUpperCase())
+        ?
+          $("#month").html(nextMonth.toUpperCase())
         : window.alert(
             "Done for the FY! Please reload the page to enter new data."
           );
@@ -67,28 +110,35 @@ $(document).ready(() => {
 
   $("#addToTable_btn").click(function () {
     // const month = $("#month").val();
-      const month = $("#month").html(); //change for making month uneditable
-      console.log("adding row to table", month);
-      const index = months.findIndex((ele) => ele.month.toLowerCase() === month.toLowerCase()); //change for making month uneditable
-      if (!months[index].isAddedToTable) {
-        if(window.confirm("Are you sure you wnat to add this row to table?")){
-          try {
-            opBal = parseFloat($("#opening-bal").val(), 2);
-            const bfr_15 = parseFloat($("#before-15").val() || 0, 2);
-            const aft_15 = parseFloat($("#after-15").val() || 0, 2);
-            const intPA = parseFloat($("#int-pa").val(), 2);
-            const pfRecoBfr = parseFloat($("#pf-reco-bfr").val() || 0, 2);
-            const pfAdvBfr = parseFloat($("#pf-adv-bfr").val() || 0, 2);
-            const pfRecoAft = parseFloat($("#pf-reco-aft").val() || 0, 2);
-            const pfAdvAft = parseFloat($("#pf-adv-aft").val() || 0, 2);
-            const lwstPayment = opBal + bfr_15 + pfRecoBfr - pfAdvBfr;
-            const intr = (lwstPayment * intPA) / 1200;
-            intrSum += intr;
-            clBal = opBal + bfr_15 + aft_15 + pfRecoBfr + pfRecoAft - pfAdvBfr - pfAdvAft;
-            totalSum = clBal + intrSum;
-            console.log({ clBal, opBal, intr, bfr_15, aft_15 });
-            $("#PF_Table tr:last").before(
-              `<tr id=${month.toLowerCase()}>
+    const month = $("#month").html(); //change for making month uneditable
+    const index = months.findIndex(
+      (ele) => ele.month.toLowerCase() === month.toLowerCase()
+    ); //change for making month uneditable
+    if (!months[index].isAddedToTable) {
+      if (window.confirm("Are you sure you wnat to add this row to table?")) {
+        try {
+          opBal = parseFloat($("#opening-bal").val(), 2);
+          const bfr_15 = parseFloat($("#before-15").val() || 0, 2);
+          const aft_15 = parseFloat($("#after-15").val() || 0, 2);
+          const intPA = parseFloat($("#int-pa").val(), 2);
+          const pfRecoBfr = parseFloat($("#pf-reco-bfr").val() || 0, 2);
+          const pfAdvBfr = parseFloat($("#pf-adv-bfr").val() || 0, 2);
+          const pfRecoAft = parseFloat($("#pf-reco-aft").val() || 0, 2);
+          const pfAdvAft = parseFloat($("#pf-adv-aft").val() || 0, 2);
+          const lwstPayment = opBal + bfr_15 + pfRecoBfr - pfAdvBfr;
+          const intr = (lwstPayment * intPA) / 1200;
+          intrSum += intr;
+          clBal =
+            opBal +
+            bfr_15 +
+            aft_15 +
+            pfRecoBfr +
+            pfRecoAft -
+            pfAdvBfr -
+            pfAdvAft;
+          totalSum = clBal + intrSum;
+          $("#PF_Table tr:last").before(
+            `<tr id=${month.toLowerCase()}>
               <td>${month.toUpperCase()}</td>
               <td>${opBal}</td>
               <td>${bfr_15}</td>
@@ -102,18 +152,24 @@ $(document).ready(() => {
               <td>${intr.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]}</td>
               <td>${clBal.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]}</td>
               </tr>`
-            ); // two places after decimal truncation is handled using string regex matcher
-            months[index].isAddedToTable = true;
-// populate total row
-            $("#closing-bal").html(clBal);
-            $("#total-int-sum").html(intrSum.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]);
-            $("#total-bal").html(totalSum.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]);
-// populate total row End
-          } catch (err) {
-            window.alert("Something went WRONG! Please check if all fields are filled.");
-            // console.error(err);
-          }
+          ); // two places after decimal truncation is handled using string regex matcher
+          months[index].isAddedToTable = true;
+          // populate total row
+          $("#closing-bal").html(clBal);
+          $("#total-int-sum").html(
+            intrSum.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]
+          );
+          $("#total-bal").html(
+            totalSum.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]
+          );
+          // populate total row End
+        } catch (err) {
+          window.alert(
+            "Something went WRONG! Please check if all fields are filled."
+          );
+          // console.error(err);
         }
-      } else window.alert(`${month.toUpperCase()} data already added to table, please go to next month.`);
+      }
+    } else window.alert(`${month.toUpperCase()} data already added to table, please go to next month.`);
   });
 });
